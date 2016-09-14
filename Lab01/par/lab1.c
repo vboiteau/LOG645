d8513e526951b770183e185d60280b19e4d94685
Vincent Boiteau-Robert <vincent@decksign.com>
2016-09-14 19:03:16 -0400

    Add a process size value

--- Lab01/par/lab1.c
+++ Lab01/par/lab1.c
@@ -13,13 +13,15 @@ int main(int args,char *argv[])
 	int p = atoi(argv[2]);
 	int n = atoi(argv[3]);
 	int rank;
+	int size;
 	MPI_Init(&args, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	if(c == 1)
 	{
 		
 	}	
-	printf("Process %d\nTest: %i, %i, %i\n\n", rank, c, p, n);
+	printf("Process %d of %d\nTest: %i, %i, %i\n\n", rank+1, size, c, p, n);
 	MPI_Finalize();
 	return 0;
 }
