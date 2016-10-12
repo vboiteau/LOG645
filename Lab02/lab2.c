f348508fa276c7b1e442879ad48b24f9aa4e525b
Vincent Boiteau-Robert <vincent@decksign.com>
2016-10-12 06:30:06 -0400

    Add func to print

--- Lab02/lab2.c
+++ Lab02/lab2.c
@@ -4,9 +4,23 @@
 #include <omp.h>
 #include "sys/time.h"
 
+int columns = 10;
+int rows = 10;
+
+void printXY(int m[columns][rows]){
+    int i,j;
+    for (i = 0; i < rows; ++i) {
+        for (j = 0; j < columns; j++) {
+            printf("%d\t",m[i][j]);
+        }
+	printf("\n");
+    }
+    printf("\n");
+}
+
 int main(int args,char *argv[])
 {
-    double timeStart, timeEnd, Texec;
+    double TSeqExec, timeStart, timeEnd, Texec;
     struct timeval tp;
     gettimeofday (&tp, NULL); // Debut du chronometre
     timeStart = (double) (tp.tv_sec) + (double) (tp.tv_usec) / 1e6;
@@ -15,23 +29,46 @@ int main(int args,char *argv[])
 	printf("Il faut absolument avoir 3 arguments\n");
 	return 1;
     }
-    int columns = 10;
-    int rows = 10;
     int c = atoi(argv[1]);
     int p = atoi(argv[2]);
     int n = atoi(argv[3]);
+    int m0[n+1][rows][columns];
     int m1[n+1][rows][columns];
+    m0[0][0][0]=p;
+    m1[0][0][0]=p;
     //Initialisation de la premiere matrice
-    int i,j,k,suite;
+    int i,j,k;
     if(c == 1)
     {
-	suite = 0;
+	gettimeofday (&tp, NULL); // Debut du chronometre
+	timeStart = (double) (tp.tv_sec) + (double) (tp.tv_usec) / 1e6;
+	for (k = 0; k < n+1; k++) {
+	    /*printf("altération %d\n",k);*/
+	    //Calcule de la deuxieme matrice
+	    for(i=0;i < rows; i++)
+	    {
+		for(j=0;j<columns;j++)
+		{
+		    usleep(50000);
+		    if(k>0){
+			m0[k][i][j] = p + (i + j)*k;
+		    } else {
+			m0[k][i][j]=p;
+		    }
+		}
+	    }
+	}
+	gettimeofday (&tp, NULL); // Debut du chronometre
+	timeEnd = (double) (tp.tv_sec) + (double) (tp.tv_usec) / 1e6;
+	TSeqExec = timeEnd - timeStart; //Temps d'execution en secondes
+	gettimeofday (&tp, NULL); // Debut du chronometre
+	timeStart = (double) (tp.tv_sec) + (double) (tp.tv_usec) / 1e6;
+#pragma omp parallel
+	{
+#pragma omp for collapse(3) 
 	    for (k = 0; k < n+1; k++) {
 		/*printf("altération %d\n",k);*/
 		//Calcule de la deuxieme matrice
-#pragma omp parallel
-	{
-#pragma omp for collapse(2) 
 		for(i=0;i < rows; i++)
 		{
 		    for(j=0;j<columns;j++)
@@ -46,16 +83,9 @@ int main(int args,char *argv[])
 		}
 	    }
 	}
-	for (k = 0; k < n+1; k++) {
-	    for(i=0;i < rows; i++)
-	    {
-		for(j=0;j< columns;j++)
-		{
-		    printf("%d\t",m1[k][i][j]);
-		}
-		printf("\n");
-	    }
-	}
+	gettimeofday (&tp, NULL); // Debut du chronometre
+	timeEnd = (double) (tp.tv_sec) + (double) (tp.tv_usec) / 1e6;
+	Texec = timeEnd - timeStart; //Temps d'execution en secondes
     }
     if(c == 2)
     {
@@ -65,7 +95,7 @@ int main(int args,char *argv[])
 	    {
 		for(j=columns-1;j>=0;j--)
 		{
-		    usleep(50000);
+		    usleep(1000);
 		    if(k>0){
 			if(j==9)
 			{
@@ -91,9 +121,13 @@ int main(int args,char *argv[])
 	    }
 	}
     }	
-    gettimeofday (&tp, NULL); // Fin du chronometre
-    timeEnd = (double) (tp.tv_sec) + (double) (tp.tv_usec) / 1e6;
-    Texec = timeEnd - timeStart; //Temps d'execution en secondes
-    printf("%f secs\n", Texec);
+    printf("%s\n","first of seq");
+    printXY(&m0[0][0][0]);
+    printf("%s\n","last of seq");
+    printXY(&m0[n][0][0]);
+    printf("%s\n","last of par");
+    printXY(&m1[n][0][0]);
+    float S = TSeqExec/Texec;
+    printf("seq: %f secs, par: %f secs, S:%f, E:%f\n", TSeqExec, Texec,S,(S/64));
     return 0;
 }
