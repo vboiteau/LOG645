2080dd961b033752f29ab1c78dfddf4946416594
Vincent Boiteau-Robert <vincent@decksign.com>
2016-10-19 15:48:36 -0400

    Init matrix and temps d'attentes seqeuentielle

--- Lab03/main.c
+++ Lab03/main.c
@@ -20,12 +20,16 @@ int main(int args,char *argv[])
     float td = atof(argv[4]);
     float h = atof(argv[5]);
     printf("Arguments Values\nm\tn\tnp\ttd\th\n%d\t%d\t%d\t%.2f\t%.2f\n\n",m,n,np,td,h);
+    int USeq[np][m][n];
+    int UPar[np][m][n];
     int rank;
     int size;
+    int TEMPS_ATTENTES=5;
     double timeStart, timeEnd, TSeqExec, TParExec;
     MPI_Init(&args, &argv);
     gettimeofday (&tp, NULL); // Debut du chronometre
     timeStart = (double) (tp.tv_sec) + (double) (tp.tv_usec) / 1e6;
+    
     // Start of programme code
     MPI_Comm_size(MPI_COMM_WORLD,&size);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
