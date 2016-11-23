8d3867bd9e42886e0d5ff9cff393f2d69ee427df
Boiteau-Robert <AM61440@ens.etsmtl.ca>
2016-11-23 17:36:30 -0500

    Push initial lab4

---
+++ Lab4/Lab4/main.cpp
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+#define MEM_SIZE (128)
+#define MAX_SOURCE_SIZE (0x100000)
+
+int main()
+{
+	cl_device_id device_id = NULL;
+	cl_context context = NULL;
+	cl_command_queue command_queue = NULL;
+	cl_mem memobj = NULL;
+	cl_program program = NULL;
+	cl_kernel kernel = NULL;
+	cl_platform_id platform_id = NULL;
+	cl_uint ret_num_devices;
+	cl_uint ret_num_platforms;
+	cl_int ret;
+
+	char string[MEM_SIZE];
+
+	FILE *fp;
+	char fileName[] = "./kernel.cl";
+	char *source_str;
+	size_t source_size;
+
+	/* Load the source code containing the kernel*/
+	fopen_s(&fp,fileName, "r");
+	if (!fp) {
+		fprintf(stderr, "Failed to load kernel.\n");
+		exit(1);
+	}
+	source_str = (char*)malloc(MAX_SOURCE_SIZE);
+	source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
+	fclose(fp);
+
+	/* Get Platform and Device Info */
+	ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
+
+	/* Create OpenCL context */
+	context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
+
+	/* Create Command Queue */
+	command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
+
+	/* Create Memory Buffer */
+	int i, j;
+	float *A;
+	float *B;
+	float *C;
+
+	A = (float *)malloc(4 * 4 * sizeof(float));
+	B = (float *)malloc(4 * 4 * sizeof(float));
+	C = (float *)malloc(4 * 4 * sizeof(float));
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			A[i * 4 + j] = i * 4 + j + 1;
+			B[i * 4 + j] = j * 4 + i + 1;
+		}
+	}
+	cl_mem Amobj = NULL;
+	cl_mem Bmobj = NULL;
+	cl_mem Cmobj = NULL;
+	//memobj = clCreateBuffer(context, CL_MEM_READ_WRITE, MEM_SIZE * sizeof(char), NULL, &ret);
+	Amobj = clCreateBuffer(context, CL_MEM_READ_WRITE, 4 * 4 * sizeof(float), A, &ret);
+	Bmobj = clCreateBuffer(context, CL_MEM_READ_WRITE, 4 * 4 * sizeof(float), B, &ret);
+	Cmobj = clCreateBuffer(context, CL_MEM_READ_WRITE, 4 * 4 * sizeof(float), NULL, &ret);
+	program = clCreateProgramWithSource(context, 1, (const char **)&source_str,
+		(const size_t *)&source_size, &ret);
+
+	/* Build Kernel Program */
+	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+
+	/* Create OpenCL Kernel */
+	kernel = clCreateKernel(program, "dataParallel", &ret);
+
+	/* Set OpenCL Kernel Parameters */
+	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&Amobj);
+	ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&Bmobj);
+	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&Cmobj);
+	size_t global_item_size = 4;
+	size_t local_item_size = 1;
+	/* Execute OpenCL Kernel */
+	ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL,
+		&global_item_size, &local_item_size, 0, NULL, NULL);
+
+	/* Copy results from the memory buffer */
+	ret = clEnqueueReadBuffer(command_queue, Cmobj, CL_TRUE, 0, 4 * 4 * sizeof(float), C, 0, NULL, NULL);
+	
+
+	/* Display Result */
+	/* Display Results */
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			printf("%7.2f ", C[i * 4 + j]);
+		}
+		printf("\n");
+	}
+
+	/* Finalization */
+	ret = clFlush(command_queue);
+	ret = clFinish(command_queue);
+	ret = clReleaseKernel(kernel);
+	ret = clReleaseProgram(program);
+	ret = clReleaseMemObject(memobj);
+	ret = clReleaseMemObject(Amobj);
+	ret = clReleaseMemObject(Bmobj);
+	ret = clReleaseMemObject(Cmobj);
+	ret = clReleaseCommandQueue(command_queue);
+	ret = clReleaseContext(context);
+
+	free(source_str);
+	free(A);
+	free(B);
+	free(C);
+	return 0;
+}
